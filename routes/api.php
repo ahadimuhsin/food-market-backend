4f4d395c8d45229bb2db8cb718ef760444a83b60
Muhsin Ahadi <muhsin_ahadi@student.uns.ac.id>
2021-02-04 21:35:23 +0700

    add models, controllers, and dependencies

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', 'UserController@login');
 Route::post('register', 'UserController@register');
+Route::get('food', 'API\FoodController@all');
+
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
    Route::get('user', 'UserController@fetch');
    Route::post('update-profile', 'UserController@updateProfile');
    Route::post('user/photo', 'UserController@updatePhoto');
+   Route::get('transaction', 'API\TransactionController@all');
+   Route::post('transaction/{id}', 'API\TransactionController@update');
    Route::post('logout', 'UserController@logout');
 });
